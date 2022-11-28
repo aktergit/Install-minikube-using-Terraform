647ce4bc950a966d982c5c33c374b4dba357b22c
aktergit <94663914+aktergit@users.noreply.github.com>
2022-11-27 20:31:00 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ca-central-1"
+  access_key = "***"
+  secret_key = "***"
+}
